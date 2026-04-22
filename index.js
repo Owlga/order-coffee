392c9dcd5387670f8759b65e6a0f2bd5c49d7fd9
Owlga <olgau2ekb@gmail.com>
2026-04-22 14:19:52 +0500

    2

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
 let totalOrderCount = 1;
 let currentOrderCount = 1;
 
+function closeOrder(closeButton) {
+    console.log(currentOrderCount);
+    if (currentOrderCount !== 1) {
+        currentOrderCount -= 1;
+        const fieldSet = closeButton.parentElement;
+        if (fieldSet) {
+            const formToRemove = fieldSet.parentElement;
+            formToRemove.remove();
+
+        }
+    }
+}
+
 function addOrder() {
     const form = document.querySelector('form');
     const newForm = form.cloneNode(true);
     const currentH = newForm.querySelector('h4');
     totalOrderCount += 1;
+    currentOrderCount += 1;
     currentH.textContent = `Напиток №${totalOrderCount}`;
+
     const newButton = newForm.querySelector('.add-button');
     newButton.onclick = addOrder;
-    document.body.appendChild(newForm);
 
+    const closeButton = newForm.querySelector('.close-button');
+    closeButton.onclick = (event) => {
+        closeOrder(closeButton);
+    }
+
+    document.body.appendChild(newForm);
 }
 const button = document.body.querySelector('.add-button');
 button.onclick = addOrder;
 
-
+const closeButtons = document.getElementsByClassName('close-button');
+for (const button of closeButtons) {
+    button.onclick = (event) => {
+        closeOrder(button);
+    }
+}
